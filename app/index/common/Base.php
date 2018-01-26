bf94c01e23bcf97969576d25219faad49108633e
FerreWagner <18408229270@163.com>
2018-01-26 17:47:29 +0800

    cate-data prepare

--- app/index/common/Base.php
+++ app/index/common/Base.php
@@ -5,9 +5,25 @@ use think\Controller;
 
 class Base extends Controller
 {
-    public function cate()
-    {
-//         db('ca')
+    
+    /**
+     * 获取前台导航栏
+     * TODO:前台对应order值进行循环
+     */
+    public function cate(){
+        $cateres   = db('category')->where(['pid' => 0])->select(); //顶级分类的pid=0
+        foreach ($cateres as $k => $v){
+            $child = db('category')->where(['pid' => $v['id']])->order('sort', 'asc')->select();
+            if($child){    //如果存在子级栏目
+                $cateres[$k]['child'] = $child;
+            }else{
+                $cateres[$k]['child'] = 0;
+            }
+        }
+        
+        $this->view->assign('cateres',$cateres);
     }
+    
+    
     
 }
---
+++ app/index/model/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\index\model;
+
+use think\Model;
+
+class Index extends Model
+{
+    // 定义类型转换
+//     protected $type = [
+//         'create_time'    => 'timestamp:Y-m-d H:i:s',
+//         'update_time'    => 'timestamp:Y-m-d H:i:s',
+//     ];
+    
+    
+}
