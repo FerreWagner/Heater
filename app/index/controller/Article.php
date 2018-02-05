5d1491c7c0b15a9441c13fd69ce2446e8d570b10
FerreWagner <18408229270@163.com>
2018-02-05 11:02:27 +0800

    design detail success

--- app/index/controller/Article.php
+++ app/index/controller/Article.php
@@ -9,6 +9,11 @@ class Article extends Base
      * TIP:存放除products外所有的文章分类
      */
     
-    
+    public function design()
+    {
+        $design = db('article')->field('id, title, content')->find(input('id'));
+        $this->view->assign('design', $design);
+        return $this->view->fetch('design');
+    }
     
 }
