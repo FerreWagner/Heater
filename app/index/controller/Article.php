707ba3dd78a3bc5ceeca2db62b97232167f5089d
FerreWagner <18408229270@163.com>
2018-02-06 15:37:23 +0800

    restru catesearch && blog success && search article success

--- app/index/controller/Article.php
+++ app/index/controller/Article.php
@@ -3,6 +3,9 @@ namespace app\index\controller;
 
 use app\index\common\Base;
 use app\index\model\Article as ArticleModel;
+use app\index\model\Category as IndexCate;
+
+use think\Request;
 
 class Article extends Base
 {
@@ -10,6 +13,47 @@ class Article extends Base
      * TIP:存放除products外所有的文章分类
      */
     
+    
+    /**
+     * cate搜索页
+     * @return string
+     */
+    public function cateSearch()
+    {
+        $id = input('id');
+    
+        //多页面和单页面
+        if (!in_array($id, config('index_module.singlepage'))){
+            $cate    = new IndexCate();
+            $cate_id = $cate->getCate($id);
+    
+            $art = db('article')
+                 ->field('a.id,a.thumb,a.desc,b.catename')
+                 ->alias('a')
+                 ->join('heater_category b','a.cate=b.id')
+                 ->order('a.order desc')
+                 ->where('b.id', 'in', $cate_id)
+                 ->paginate(config('index_module.propage'));
+        }else {
+            $art = db('article')->where('id', $id)->select();
+        }
+    
+        //catename
+        $category = db('category')->field('id, catename')->find($id);
+    
+        $this->view->assign([
+            'art'      => $art,
+            'category' => $category,
+        ]);
+    
+        $template = in_array($id, config('index_module.singlepage')) ? 'single' : 'multi';
+        return $this->view->fetch($template);
+    }
+
+    
+    /**
+     * 单页面
+     */
     public function singlePage()
     {
         $art = db('article')
@@ -23,6 +67,9 @@ class Article extends Base
         return $this->view->fetch('single-page');
     }
     
+    /**
+     * normal文章数据显示
+     */
     public function blog()
     {
         $model = new ArticleModel();
@@ -35,5 +82,45 @@ class Article extends Base
         
         return $this->view->fetch('blog');
     }
+    
+
+
+
+    /**
+     * tag/keywords/desc/title 都可以作为搜索依据
+     * @param Request $request
+     */
+    public function search(Request $request)
+    {
+        //对恶意的多个参数查询处理
+        if (count($request->param()) > 1){
+            $this->redirect('index/index/index');
+        }
+    
+        //参数值处理成字符串
+        $arr = $request->param();
+        $map = implode('', array_values($arr));
+        
+        //对tag/keywords title/desc的查询
+        if (in_array(implode('', array_keys($request->param())), ['tag', 'keywords'])){
+            $art = db('article')
+                   ->field('id,thumb,desc')
+                   ->where('tag|keywords', 'like', '%'.$map.'%')
+                   ->select();
+        }elseif (implode('', array_keys($request->param())) == 'search'){
+            $art = db('article')
+                   ->field('id,thumb,desc')
+                   ->where('title|desc', 'like', '%'.$map.'%')
+                   ->select();
+        }
+        
+        $this->view->assign([
+            'art' => $art,
+        ]);
+        
+        return $this->view->fetch('multi');
+    }
+    
+
     
 }
--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -2,9 +2,9 @@
 namespace app\index\controller;
 
 use app\index\common\Base;
-use app\index\model\Category as IndexCate;
 use app\index\model\Article as ArticleModel;
 use app\admin\model\System;
+use app\index\model\Category as IndexCate;
 
 class Index extends Base
 {
@@ -30,7 +30,23 @@ class Index extends Base
         return $this->view->fetch('index');
     }
     
+
+    public function product()
+    {
+        $cate    = new IndexCate();
+        $pro_pid = $cate->findCateId();
     
+        $product = db('article')
+        ->field('id, title, thumb, keywords, desc')
+        ->where('cate', 'in', $pro_pid)
+        ->order('order', 'desc')
+        ->limit(6)
+        ->select();
+    
+        //TODO 1、七牛云图片显示处理 2、七牛云图片长宽处理
+    
+        $this->view->assign('product', $product);
+    }
     
     public function banner()
     {
@@ -38,58 +54,7 @@ class Index extends Base
         $this->view->assign('banner', $banner);
     }
     
-    /**
-     * cate搜索页
-     * @return string
-     */
-    public function cateSearch()
-    {
-        $id = input('id');
-        
-        //多页面和单页面
-        if (!in_array($id, config('index_module.singlepage'))){
-            $cate    = new IndexCate();
-            $cate_id = $cate->getCate($id);
-            
-            $art = db('article')
-                 ->field('a.id,a.thumb,b.catename')
-                 ->alias('a')
-                 ->join('heater_category b','a.cate=b.id')
-                 ->order('a.order desc')
-                 ->where('b.id', 'in', $cate_id)
-                 ->paginate(config('index_module.propage'));
-        }else {
-            $art = db('article')->where('id', $id)->select();
-        }
-        
-        //catename
-        $category = db('category')->field('id, catename')->find($id);
-        
-        $this->view->assign([
-            'art'      => $art,
-            'category' => $category,
-        ]);
-        
-        $template = in_array($id, config('index_module.singlepage')) ? 'single' : 'multi';
-        return $this->view->fetch($template);
-    }
-    
-    public function product()
-    {
-        $cate    = new IndexCate();
-        $pro_pid = $cate->findCateId();
-        
-        $product = db('article')
-            ->field('id, title, thumb, keywords, desc')
-            ->where('cate', 'in', $pro_pid)
-            ->order('order', 'desc')
-            ->limit(6)
-            ->select();
-        
-        //TODO 1、七牛云图片显示处理 2、七牛云图片长宽处理
-        
-        $this->view->assign('product', $product);
-    }
+
     
     
     
