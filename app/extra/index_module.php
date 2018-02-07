d61681699e68e054b36f55e14722587ce817bdd6
FerreWagner <18408229270@163.com>
2018-02-07 10:26:19 +0800

    fixed extra bug

--- app/extra/index_module.php
+++ app/extra/index_module.php
@@ -3,5 +3,8 @@
 return [
     'propage'    => 12,     //一页12product显示
     'productid'  => 2,
-    
+    'catedesign' => 9,
+    'catefeatures' => 8,
+    'cateclients'  => 10,
+    'cateblog'     => 3,
 ];
