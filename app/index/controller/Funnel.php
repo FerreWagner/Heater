1d4bae3d7a69db507adeb305b4a09af8dbb41caf
FerreWagner <18408229270@163.com>
2018-03-14 16:17:39 +0800

    product fun2 success

--- app/index/controller/Funnel.php
+++ app/index/controller/Funnel.php
@@ -43,4 +43,37 @@ class Funnel extends Products
         return $this->view->fetch(request()->action());
     }
     
+    public function fun2(Request $request)
+    {
+        $common = new Common();
+        $common->checkExcel();
+    
+        $x_arr = $x_arr1 = $y_arr = $y_arr1 = [];
+    
+        $common->getForm($request);
+    
+        $pic_view   = Cookie::get('excel_data');
+        $_title     = $common->deleteEmpty(array_pop($pic_view));
+    
+        foreach ($pic_view as $k => $v){
+            $x_arr[]  .= $v[0];
+            $y_arr[]  .= $v[1];
+            $x_arr1[] .= $v[2];
+            $y_arr1[] .= $v[3];
+        }
+        
+        $fir_data = array_combine($y_arr, $x_arr);
+        $sec_data = array_combine($y_arr1, $x_arr1);
+        
+        $this->view->assign([
+            'x_arr'     => $x_arr,
+            'x_arr1'    => $x_arr1,
+            'f_data'    => $fir_data,
+            's_data'    => $sec_data,
+            'bar_title' => $_title,
+        ]);
+    
+        return $this->view->fetch(request()->action());
+    }
+    
 }
